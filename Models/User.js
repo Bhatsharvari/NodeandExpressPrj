2c2dc3b3e5a5ecba555a41438bd9742b89fadfa7
Bhatsharvari <123368186+Bhatsharvari@users.noreply.github.com>
2023-07-18 17:24:00 +0530

    Add files via upload

---
+++ Models/User.js
@@ -0,0 +1,21 @@
+const {Schema,model}=require("mongoose")
+
+const userSchema=new Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    email:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true
+    },
+})
+
+module.exports=model("user",userSchema)
